45413d8c97830fbeb2838e855aaa9f5a086a600d
kk <k.klapczynski@gmail.com>
2020-01-24 12:02:31 +0100

    Server's second endpoint localhost:8001/testImage returning a svg image.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,10 +2,11 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-
+import java.nio.file.Files;
 
 public class App {
     public static void main(String[] args) throws IOException {
@@ -21,9 +22,24 @@ public class App {
             }
         }
 
+        class ImageFileHandler implements HttpHandler {
+            public void handle(HttpExchange t) throws IOException {
+                File file = new File("src/main/resources/europe_map.svg");
+//                Path path = Paths.get("src/main/resources/Lion-vector.svg");
+////              System.out.println("path: " + path.toString());
+//                Boolean fileNotExists = Files.notExists(path);
+//                System.out.println("File not exists?: " + fileNotExists);
+                t.sendResponseHeaders(200, file.length());
+                OutputStream os = t.getResponseBody();
+                Files.copy(file.toPath(), os);
+                os.close();
+            }
+        }
+
         int port = 8001;
         HttpServer server =  HttpServer.create(new InetSocketAddress(port),0);
         server.createContext("/test", new FirstHttpHandler());
+        server.createContext("/testImage", new ImageFileHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
         System.out.println("End1");
